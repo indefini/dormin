6753606d0c1c316a60b703739fc87a5860df8dcd
indefini <chris@indefini.org>
2016-02-25 05:52:12 +0900

    add libgles2 link

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,93 @@
+#![feature(box_syntax)]
+#![feature(core)]
+#![feature(convert)]
+#![feature(step_by)]
+#![feature(zero_one)]
+
+#![feature(vec_push_all)]
+#![feature(borrow_state)]
+
+//TODO remove
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(improper_ctypes)]
+//#![allow(ctypes)]
+
+#![feature(plugin)]
+//#![plugin(clippy)]
+
+extern crate rustc_serialize;
+extern crate byteorder;
+extern crate libc;
+//extern crate sync;
+extern crate png;
+extern crate toml;
+//extern crate debug;
+extern crate uuid;
+extern crate core;
+
+#[macro_use]
+extern crate lazy_static;
+
+#[macro_use]
+extern crate lua;
+
+//use serialize::{json, Encodable, Encoder, Decoder, Decodable};
+use std::collections::HashMap;
+use std::sync::{RwLock, Arc};
+//use std::rc::Rc;
+//use std::cell::RefCell;
+use std::mem;
+//use std::any::{Any, AnyRefExt};
+
+use std::io::{self, Write};
+use std::path::Path;
+use std::process;
+
+
 #[cfg(test)]
 mod test {
     #[test]
     fn it_works() {
     }
 }
+
+#[macro_use]
+mod property;
+
+pub mod resource;
+mod shader;
+mod material;
+mod armature;
+mod mesh;
+//mod mesh_render;
+pub mod render;
+mod object;
+mod uniform;
+mod matrix;
+mod vec;
+mod camera;
+pub mod scene;
+mod texture;
+mod geometry;
+mod intersection;
+mod fbo;
+pub mod factory;
+
+mod transform;
+
+mod model;
+
+mod component;
+use component::manager;
+
+mod util;
+
+#[link(name = "GLESv2", kind="dylib")]
+#[link(name = "cypher")]
+extern
+{
+    pub fn cgl_clear() -> ();
+}
+
