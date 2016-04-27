5dae0953434fcf77b21f7cb290ed3cde8a3c5d1a
Christophe Sadoine <chris@indefini.org>
2016-04-27 19:31:27 +0900

    removed old feature

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,8 @@
 //#![feature(trace_macros)]
 //#![feature(slicing_syntax)]
 #![feature(box_syntax)]
-#![feature(core)]
-#![feature(convert)]
 #![feature(step_by)]
 #![feature(zero_one)]
-
-#![feature(vec_push_all)]
-#![feature(borrow_state)]
 
 //TODO remove
 #![allow(unused_variables)]
